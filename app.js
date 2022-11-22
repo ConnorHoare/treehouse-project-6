7b2760124255e405bbe25b47e4b14b92cc431d8d
ConnorHoare <connor@tipcigroup.com>
2022-11-22 12:42:42 +0000

    update

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/static', express.static(path.join(__dirname, '/public')));
+
 
 app.get("/", (req, res) => {
   res.render('index');
